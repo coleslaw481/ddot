f8d591a310016208ad4dc69068ac436b0f80f7e8
Akshat Singhal <a2singhal@ucsd.edu>
2019-07-23 14:56:39 -0700

    Adding POST call

---
+++ rest/cd_api.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from ddot import controller
+from ddot.controller import Controller
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Controller, '/cd')
+app.run(debug=True)
